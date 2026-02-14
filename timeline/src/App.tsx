8cf406652a7428b27542924425976cee88c7bf90
whoevertf <nestpaul1569@gmail.com>
2026-02-14 23:57:24 +0300

    register stuff

--- timeline/src/App.tsx
+++ timeline/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./app.css";
+import "./App.css";
 import { Timeline, type TimelineRow, type TimelineTask } from "./components/Timeline";
 
 function startOfDay(d: Date) {
